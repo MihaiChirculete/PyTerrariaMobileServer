a051249f5b851cf6ce67948dc6b220c47cd6fcf9
Chalapa13 <mihai.chirculete98@gmail.com>
2020-10-19 11:30:34 +0300

    Added placeholder class `SetFactory`

---
+++ terraria/id/set_factory.py
@@ -0,0 +1,28 @@
+class SetFactory:
+    def __init__(self, size):
+        self._size = size
+        self._int_buffer_cache = [[]]  # private Queue<int[]> _intBufferCache = new Queue<int[]>();
+        self._bool_buffer_cache = [[]]  # private Queue<int[]> _intBufferCache = new Queue<int[]>();
+        self._queue_lock = object()
+
+    def get_bool_buffer(self):
+        return self._bool_buffer_cache
+
+    def get_int_buffer(self):
+        return self._int_buffer_cache
+
+    def recycle(self, buffer):
+        # TO-DO: Port code
+        pass
+
+    def create_bool_set(self, types, default_state = None):
+        # TO-DO: Port code
+        pass
+
+    def create_int_set(self, default_state, inputs):
+        # TO-DO: Port code
+        pass
+
+    def create_custom_set(self, default_state, inputs):
+        # TO-DO: Port code
+        pass
