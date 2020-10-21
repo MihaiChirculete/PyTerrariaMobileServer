76b1a1a695768e3294562f8d31ecbb9916512a7b
Chalapa13 <mihai.chirculete98@gmail.com>
2020-10-21 20:43:13 +0300

    Added default values to fields

--- terraria/entity.py
+++ terraria/entity.py
@@ -1,18 +1,18 @@
 class Entity:
     def __init__(self):
-        self.who_am_i: int
-        self.active: bool
+        self.who_am_i: int = None
+        self.active: bool = None
         # self.position :Vector2
         # self.velocity :Vector2
         # self.old_position :Vector2
         # self.old_velocity :Vector2
-        self.old_direction: int
+        self.old_direction: int = None
         self.direction = 1
-        self.width: int
-        self.height: int
-        self.wet: bool
-        self.honey_wet: bool
-        self.wet_count: int # was byte in original
-        self.lava_wet: bool
+        self.width: int = None
+        self.height: int = None
+        self.wet: bool = None
+        self.honey_wet: bool = None
+        self.wet_count: int = None # was byte in original
+        self.lava_wet: bool = None
 
 # TO-DO: PORT THE METHODS
